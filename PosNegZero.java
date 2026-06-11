246fdeb98fdc0c669ad8c7f2993703730a79e3d4
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-11 18:53:56 +0530

    Add files via upload

---
+++ PosNegZero.java
@@ -0,0 +1,19 @@
+
+public class PosNegZero {
+
+	public static void main(String[] args) {
+		int a = 0;
+		
+		if (a > 0) {
+			System.out.println("Positive");
+		}
+		else if (a < 0) {
+			System.out.println("Negative");
+		} else {
+			System.out.println("Zero");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
