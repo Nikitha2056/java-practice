a837cc528d6e2e62386cd665fab1a5fba38ba173
Nikitha2056 <nikithasri9631@gmail.com>
2026-06-11 18:56:26 +0530

    Add files via upload

---
+++ DivisibilityCheck.java
@@ -0,0 +1,20 @@
+
+public class DivisibilityCheck {
+
+	public static void main(String[] args) {
+		int number = 140;
+
+        if (number % 4 == 0 && number % 5 == 0 && number % 7 == 0) {
+
+            System.out.println("Divisible by 4, 5 and 7");
+
+        } else {
+
+            System.out.println("Not Divisible");
+
+        }
+		// TODO Auto-generated method stub
+
+	}
+
+}
